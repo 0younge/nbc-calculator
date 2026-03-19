1f4b2dafd72c6366c4d1543aea88e929bd6c188e
0younge <s45628439@gmail.com>
2026-03-19 16:43:01 +0900

    add: Calculator class calculate

--- src/calculatorplus/Calculator.java
+++ src/calculatorplus/Calculator.java
@@ -1,4 +1,36 @@
 package calculatorplus;
 
+import java.util.ArrayList;
+
 public class Calculator {
+
+    // 속성
+    ArrayList<Integer> resultList = new ArrayList<>();
+
+    // 생성자
+
+    // 기능
+    public int calculate(int num1, int num2, String symbol) {
+        int result;
+        switch (symbol) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    throw new RuntimeException();
+                } else result = num1 / num2;
+                break;
+            default:
+                throw new RuntimeException();
+        }
+        resultList.add(result);
+        return result;
+    }
 }
