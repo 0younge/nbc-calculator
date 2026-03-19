19ea112b7b1bacfb784e76a88162682d95ef8859
0younge <s45628439@gmail.com>
2026-03-19 17:21:50 +0900

    rm: Main / add: remove old result

--- src/calculatorplus/App.java
+++ src/calculatorplus/App.java
@@ -21,6 +21,7 @@ public class App {
         int result = 0; // 결과 값
         String keepCalculator; // 반복 계산 여부 확인
         boolean keep  = true; // 반복 계산 처리 값
+        String removeOldResult;
 
         while (keep) {
             // 첫 번째 정수 입력
@@ -79,6 +80,18 @@ public class App {
             // 출력 부분
             System.out.println("계산 결과는: " + result + "입니다.");
 
+            // 게터 활용
+            System.out.println("현재까지 계산 결과(오래된 순): " + calculator.getResultList());
+
+            // 오래된 기록 지우기
+            System.out.println("가장 오래된 계산 결과를 지우시겠습니까?");
+            System.out.print("yes 입력시 가장 오래된 계산 결과 삭제/ 그 외 입력시 취소: ");
+            removeOldResult = sc.nextLine();
+            if (removeOldResult.equals("yes")) {
+                calculator.removeOldResultList();
+            }
+            System.out.println("현재까지 계산 결과(오래된 순): " + calculator.getResultList());
+
             // 반복 계산 여부 확인
             System.out.println("exit를 입력하시면 계산을 정지합니다 그 외 문자 입력시 계산이 다시 진행됩니다.");
             System.out.print("입력하세요: ");
@@ -87,8 +100,7 @@ public class App {
                 keep = false;
             }
 
-            // 게터 활용
-            System.out.println("현재까지 계산 결과: " + calculator.getResultList());
+
         }
 
     }
