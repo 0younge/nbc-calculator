3a7d5cf6ae8a0593a8058bfa3c1c4864a180e612
0younge <s45628439@gmail.com>
2026-03-19 16:51:22 +0900

    fix: calculate

--- src/calculatorplus/Calculator.java
+++ src/calculatorplus/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
 
     // 기능
     public int calculate(int num1, int num2, String symbol) {
-        int result;
+        int result = 0;
         switch (symbol) {
             case "+":
                 result = num1 + num2;
@@ -23,12 +23,8 @@ public class Calculator {
                 result = num1 * num2;
                 break;
             case "/":
-                if (num2 == 0) {
-                    throw new RuntimeException();
-                } else result = num1 / num2;
+                result = num1 / num2;
                 break;
-            default:
-                throw new RuntimeException();
         }
         resultList.add(result);
         return result;
