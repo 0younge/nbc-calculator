a8efd790b3ae886b7b121bbafa5b1c1c27c4b5a3
0younge <s45628439@gmail.com>
2026-03-19 12:31:24 +0900

    add: firstNum

--- src/calculatorbasic/Main.java
+++ src/calculatorbasic/Main.java
@@ -1,8 +1,42 @@
 package calculatorbasic;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("=== 계산기 ===");
+
+        // 1. 스캐너 생성
+        Scanner sc = new Scanner(System.in);
+
+        int firstNum; // 첫 번째 값
+        int secondNum; // 두 번째 값
+        String symbol; // 기호
+        int result = 0; // 결과 값
+        String keepCalculator; // 반복 계산 여부 확인
+        boolean keep  = true; // 반복 계산 처리 값
+
+        // 첫 번째 정수 입력
+        while (true) {
+
+            System.out.print("첫 번째 정수를 입력하세요: ");
+            try {
+                firstNum = sc.nextInt();
+                sc.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("정수를 입력하세요.");
+                sc.nextLine();
+                continue;
+            }
+
+        }
+
+
+
 
 
 
